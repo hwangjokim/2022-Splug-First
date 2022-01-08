ea0f47df72ef8a99538e087209223b0ec94deca3
hwangjokim <hg7258@naver.com>
2022-01-08 23:39:42 +0900

    유저 검색 구현

--- splug_test.c
+++ splug_test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 typedef struct info{
 	char name[20];
@@ -12,22 +12,31 @@ typedef struct info{
 void print_main(void);
 info* load_data(FILE *fp);
 void print_user(info *user);
+void search_user(info *user); 
 
 int main(void){
+	print_main();
+	printf("=======================\n");
 	while(1){
 		int select;
 		FILE *fp=fopen("명단.txt","r");
 		info *user=load_data(fp);
-		print_main();
+		//sorting_node(user);
+
 		printf("선택 : ");
+
 		scanf("%d",&select);
+		printf("=======================\n");
 		switch(select){
 			case(1):
 				print_user(user);
+				printf("=======================\n");
 				break;
 			case(2):
 				break;
 			case(3):
+				search_user(user);
+				printf("=======================\n");
 				break;
 			case(4):
 				break;
@@ -63,13 +72,37 @@ info* load_data(FILE *fp){
 	head->next=load_data(fp);
 	return head;
 }
+/*
+   void sorting_node(info *user){	
+   info *temp=(info *)malloc(sizeof(info));
+   info *head=user;
 
+
+
+
+   }
+   */
 void print_user(info* user){
-
 	for(info *search=user;search!=NULL;search=search->next){
 		printf("%s %s %s\n",search->name,search->address,search->phone);
 	}
 
+}
+
+void search_user(info* user){
+	char search_name[30];
+	printf("찾을 회원의 이름을 입력하세요: ");
+	scanf("%s",search_name);
+	while(user!=NULL){
+		if (!strcmp(search_name,user->name)){
+			printf("이름 : %s\n",user->name);
+			printf("주소 : %s\n",user->address);
+			printf("전화번호 : %s\n",user->phone);
+			return ;
+		}
+		user=user->next;
+	}
+	printf("찾는 회원은 존재하지 않습니다.\n");
 
 
 
